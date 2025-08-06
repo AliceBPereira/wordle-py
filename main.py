b1e333444c1ddbcd5a14f6f3619a78ee90df36bc
Alice Barros Pereira <103052000+AliceBPereira@users.noreply.github.com>
2025-08-05 21:26:44 -0300

    otimizando def jogar

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ try:
     url = "https://raw.githubusercontent.com/pythonprobr/palavras/master/palavras.txt"
     resposta = requests.get(url)
     todas_as_palavras = resposta.text.splitlines()
-    # Filtra apenas palavras com 5 letras e sem hífens/números
     banco_de_dados = [p.lower() for p in todas_as_palavras if len(p) == 5 and p.isalpha()]
     print(f"Banco de dados online carregado com {len(banco_de_dados)} palavras!")
 except Exception as e:
@@ -38,19 +37,22 @@ def verificar_caracteres(palavra_secreta, palavra_usuario):
             tentativa += f"[on #666666] {palavra_usuario[i].upper()} [/]"
     return tentativa
 
-def jogar(palavra_secreta):
-    i = 0
-    tentativas = ['[on #666666] _ [/]' * 5 for i in range(5)]
+def desenhar_grade(console, tentativas):
+    console.clear()
     for t in tentativas:
-            console.print(t)
-    while i < 5:
+        console.print(t)
+
+def jogar(palavra_secreta):
+    tentativas = ['[on #666666] _ [/]' * 5 for i in range(5)]
+    
+    for i in range(5):
+        desenhar_grade(console, tentativas)
         palavra_usuario = receber_palavra()
+
         tentativa = verificar_caracteres(palavra_secreta, palavra_usuario)
-        console.clear()
-        for t in tentativas:
-            tentativas[i]= tentativa
-        for t in tentativas:
-            console.print(t)
+        tentativas[i] = tentativa
+        desenhar_grade(console, tentativas)
+
         if palavra_usuario == palavra_secreta:
             print(f"Parabéns! Você acertou a palavra! {palavra_secreta.upper()}")
             return True
